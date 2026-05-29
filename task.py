268ad3fbf972fdabd4e2853ac8b2f730d480f37b
msantella8 <m.santella8@studenti.unisa.it>
2026-05-29 11:47:53 +0200

    Sub-task 4

--- task.py
+++ task.py
@@ -28,6 +28,13 @@ def risolvi_sistema_lineare(A: list, b: list) -> np.ndarray:
 
 def correlazione_matrici(m1: list, m2: list) -> np.ndarray:
     """Sub-task 4: Correlazione tra Matrici 2x2."""
+    m1=np.array(m1)
+    m2=np.array(m2)
+    mf1=m1.flatten()
+    mf2=m2.flatten()
+    cm=np.corrcoef(mf1,mf2)
+    return cm
+
     pass
 
 def operazioni_elemento_per_elemento(v1: list) -> tuple:
@@ -39,7 +46,7 @@ def main():
     print("Sub-task 1:", prodotto_scalare([1, 2, 3], [4, 5, 6]))
     print("Sub-task 1:", rango_matrice([[1, 2], [3, 4]]))
     print("Sub-task 3:", risolvi_sistema_lineare([[2, 1], [1, 3]], [5, 7]))
-    print("Sub-task 4:", correlazione_matrici([[1, 2], [3, 4]], [[2, 4], [6, 8]]))
+    print("Sub-task 4:", correlazione_matrici([[1, 2], [3, 4]], [[4, 3], [2, 1]]))
     print("Sub-task 5:", operazioni_elemento_per_elemento([0, 0.5, 1, -0.5]))
 
 if __name__ == "__main__":
