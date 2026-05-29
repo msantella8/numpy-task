6be05ad329bdccde780dbffea653bc726d150536
msantella8 <m.santella8@studenti.unisa.it>
2026-05-29 11:58:38 +0200

    Sub-task 5

--- task.py
+++ task.py
@@ -39,6 +39,13 @@ def correlazione_matrici(m1: list, m2: list) -> np.ndarray:
 
 def operazioni_elemento_per_elemento(v1: list) -> tuple:
     """Sub-task 5: Restituisce (seno, coseno, arcoseno, arcocoseno) elemento per elemento calcolati sul primo array."""
+    v1=np.array(v1)
+    vs=np.sin(v1)
+    vc=np.cos(v1)
+    va=np.arctan(v1)
+    vac=np.arccos(v1)
+    t=[vs, vc, va, vac]
+    return t
     pass
 
 
@@ -47,7 +54,7 @@ def main():
     print("Sub-task 1:", rango_matrice([[1, 2], [3, 4]]))
     print("Sub-task 3:", risolvi_sistema_lineare([[2, 1], [1, 3]], [5, 7]))
     print("Sub-task 4:", correlazione_matrici([[1, 2], [3, 4]], [[4, 3], [2, 1]]))
-    print("Sub-task 5:", operazioni_elemento_per_elemento([0, 0.5, 1, -0.5]))
+    print("Sub-task 5:", operazioni_elemento_per_elemento([0.2, -0.8, 1.0]))
 
 if __name__ == "__main__":
     main()
